a4e7a322c8607678e20ff70aa00947c1db93b5cd
mananmpatel <mananmpatel2001@gmail.com>
2023-11-18 17:57:38 +0530

    Create Category Model

---
+++ BulkyBook/BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models;
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int DisplayOrder { get; set; }
+    public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+}
